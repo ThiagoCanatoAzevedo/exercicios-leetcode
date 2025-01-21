dfa26677ba8c0389b3c0353c36067693e5133907
Thiago Canato de Azevedo <97035376+ThiagoCanatoAzevedo@users.noreply.github.com>
2025-01-21 09:26:40 -0300

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -11,10 +11,18 @@ exists, then return its index. Otherwise, return -1.
 
 class Solution(object):
     def search(self, nums, target):
-        if(target in nums):
-            return(nums.index(target))
-        else:
-            return (-1)
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return -1
 
 # ------------------------------------------------------------------------------
 '''
